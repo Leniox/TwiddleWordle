d16660bd98c49317642162cb7fa8ceea7413ceae
Greg Aronson <garonson@gmu.edu>
2018-01-27 17:00:56 -0500

    foundations

---
+++ app/src/main/java/data/wordle/twittle/twittlewordle/IndividualTwitterComp.java
@@ -0,0 +1,13 @@
+package data.wordle.twittle.twittlewordle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualTwitterComp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_twitter_comp);
+    }
+}
