9ba9e31f4257f2fe324555fae7a3ffd9f02b688c
Greg Aronson <garonson@gmu.edu>
2018-01-27 17:09:25 -0500

    Saving

--- app/src/main/java/data/wordle/twittle/twittlewordle/Introduction.java
+++ app/src/main/java/data/wordle/twittle/twittlewordle/Introduction.java
@@ -1,13 +1,75 @@
 package data.wordle.twittle.twittlewordle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Introduction extends AppCompatActivity {
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.MessageButtonBehaviour;
+import agency.tango.materialintroscreen.SlideFragmentBuilder;
+
+public class Introduction extends MaterialIntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
+
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main_intro);
+        enableLastSlideAlphaExitTransition(false);
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.first_slide_background)
+                        .buttonsColor(R.color.first_slide_buttons)
+                        //.image(R.drawable.img_office)
+                        .title("Spatial Sentiment Analysis of Twitter Feeds")
+                        .description("Want to explore?")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("We provide indepth spatial visualization with maps");
+                    }
+                }, "Details"));
+
+        addSlide(new SlideFragmentBuilder()
+                .backgroundColor(R.color.second_slide_background)
+                .buttonsColor(R.color.second_slide_buttons)
+                .title("Want more?")
+                .description("Go on")
+                .build());
+
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.third_slide_background)
+                        .buttonsColor(R.color.third_slide_buttons)
+                        //.possiblePermissions(new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.READ_SMS})
+                        .neededPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION})
+                        //.image(R.drawable.img_equipment)
+                        .title("We need these permissions!")
+                        .description("In order to get you the best analysis from our side, we need you to offer us these permissions")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("Thanks");
+                    }
+                }, "Give Permission!"));
+
+
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.first_slide_background)
+                        .buttonsColor(R.color.first_slide_buttons)
+                        //.image(R.drawable.img_office)
+                        .title("Explore")
+                        .description("See whats important around you in real time.")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent1);
+                    }
+                }, "Let's Go"));
     }
 }
--- app/src/main/java/data/wordle/twittle/twittlewordle/MainActivity.java
+++ app/src/main/java/data/wordle/twittle/twittlewordle/MainActivity.java
@@ -1,13 +1,85 @@
 package data.wordle.twittle.twittlewordle;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ldoublem.loadingviewlib.view.LVBlock;
 
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int ANIMATION_DELAY = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean firstLogIn = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (firstLogIn) {
+
+                    //  Launch app intro
+                    Intent i = new Intent(MainActivity.this, Introduction.class);
+                    startActivity(i);
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+
+
+
+        setContentView(R.layout.activity_splash);
+        final LVBlock mLVBlock = (LVBlock) findViewById(R.id.lv_block);
+
+        mLVBlock.setViewColor(Color.rgb(245,209,22));
+        mLVBlock.setShadowColor(Color.GRAY);
+        mLVBlock.startAnim(750);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                mLVBlock.stopAnim();
+                // close this activity
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+                finish();
+
+                //mProgress.setMessage("Give Us A Moment...");
+                //mProgress.show();
+            }
+        }, ANIMATION_DELAY);
+
+
+
+
+
+
+
+        
     }
+
+
 }
